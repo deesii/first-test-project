1fcb8c632bfdbde78c11b92f76dac5a00b8e359c
deesii <donnagph@hotmail.com>
2024-02-08 17:37:00 +0000

    test for most_often updated

--- tests/test_most_often.py
+++ tests/test_most_often.py
@@ -1,3 +1,17 @@
+# In your pair, write tests for this class in a file tests/test_most_often.py. Make sure to run pytest to check whether your tests are passing.
+
+# To help guide you, think about these questions in your pair:
+
+# What functionality does each of the methods in the class have?
+# What sorts of types in the list might be testable?
+# If we were only ever dealing with integers (let's say), then what sorts of interesting list contents at instantiation and later on (after adding new) could be worth including in the tests?
+# How many different outcomes are there that are testable, from get_most_often() and do you have tests for those?
+# Do your tests cover at least all of the core functionality of the class, even if you aren't testing every type in the list or every combination of inputs?
+
+
+
+
+
 from lib.most_often import *
 #Need to make sure when init, is the list we input. e.g. empty list returns empty
 def test_instantiate_empty_list():
